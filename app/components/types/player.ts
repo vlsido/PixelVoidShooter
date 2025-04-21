47c104dc64c5084ba0c88dbfabe6e4205e6c847c
Vladislav Sidorenko <vladislavsidorenko2000@gmail.com>
2025-04-21 22:07:34 +0300

    perf: add types

---
+++ app/components/types/player.ts
@@ -0,0 +1,36 @@
+export interface PlayerProps {
+
+}
+
+export interface StatsProps {
+  /**
+  * @description Kill count 
+  */
+  kills: number;
+  /**
+  * @description Total score 
+  */
+  score: number;
+}
+
+export interface GunProps extends AmmoProps {
+  /**
+  * @description The gun type
+  */
+  type: "Pistol" | "Shotgun";
+  /**
+  * @description Damage dealt with each shot
+  */
+  damage: number;
+}
+
+export interface AmmoProps {
+  /**
+  * @description Total number of bullets 
+  */
+  totalBullets: number;
+  /**
+  * @description Current number of bullets 
+  */
+  currentBullets: number;
+}
